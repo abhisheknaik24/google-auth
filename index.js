f720d6bbbf3d7980d7d52354183ad054453ae9e1
abhisheknaik24 <development.abhi2000@gmail.com>
2023-01-18 15:39:31 +0530

    changes

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ const port = process.env.PORT || 8000;
 
 app.use(express.json());
 
-// app.use(
-//   cors({
-//     origin: process.env.CLIENT_URL,
-//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 
 app.use(
   session({
